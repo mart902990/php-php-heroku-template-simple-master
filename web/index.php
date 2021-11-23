fb5b38555ec992a9b8a8c5a542d28e816a0ebc20
mart902990 <mart902990@gmail.com>
2021-11-23 23:39:37 +0700

    add fails exit

--- web/index.php
+++ web/index.php
@@ -1,15 +1,14 @@
 <?php
+session_start();
+echo 'I\'m alive!'. PHP_EOL;
+echo ' Эту форму разработал ВЕЛИКИЙ мыслитель Илья'. PHP_EOL;
 
-echo 'I\'m alive!'.PHP_EOL;
-echo "commit init '\n'  ";
 
-echo "commit 2" .'PHP_EOL';
-echo "heroku commit test 4'\n'";
-echo "push 5'\n'";
-
-echo <<<END
-      a
-     b
-    c
-\n
-END;
+if  (!$_SESSION ['username'])
+{  echo 'облом петрович' ;
+   include '.\contact.php';
+}
+elseif ($_SESSION ['username']) {echo "привет   -" ;}
+echo '<p> добавьте проверку, если внутри сессии существует не пустая переменная с именем пользователя,
+ то просто поприветствуйте пользователя, не отображая форму и разместите ссылку на скрипт exit.php.
+  Иначе отобразите форму как было раньше.';
