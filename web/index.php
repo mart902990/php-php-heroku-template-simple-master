7dad5eddfdf434aff2aa43c30c8c24c550a62b0d
Martynenko <mart902990@gmail.com>
2021-11-23 16:08:11 +0700

    eol

--- web/index.php
+++ web/index.php
@@ -3,9 +3,10 @@
 echo 'I\'m alive!'.PHP_EOL;
 echo "commit init '\n'  ";
 
-echo "commit 2" .PHP_EOL;
+echo "commit 2" .'PHP_EOL';
 echo "heroku commit test 4'\n'";
 echo "push 5'\n'";
+
 echo <<<END
       a
      b
